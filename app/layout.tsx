24e581d2a5794a983bbd94a7130e10ead93152eb
= <emmanuelgatwech@gmail.com>
2026-05-07 21:11:04 +0300

    chore: remove icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,23 +34,6 @@ export const metadata: Metadata = {
   authors: [{ name: 'Malachite Medical Centre' }],
   creator: 'Malachite Medical Centre',
   publisher: 'Malachite Medical Centre',
-  icons: {
-    icon: [
-      {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
-      },
-    ],
-    apple: '/apple-icon.png',
-  },
 }
 
 export default function RootLayout({
